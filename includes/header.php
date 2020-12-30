079d38dc357b2c57e677ed4e72f47f54ca665673
restuhanputra <restu.hanputraa@gmail.com>
2020-12-31 00:23:00 +0700

    fix title

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Task List</title>
+    <title>CRUD PHP PDO</title>
     <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
